ad0463fe61eb40b0e661623100f83ab41c0e9cfd
Gavin Lloyd <gavinhungry@gmail.com>
2014-07-16 14:24:11 -0700

    Add command line option

--- ccs.js
+++ ccs.js
@@ -1,8 +1,14 @@
+/**
+ * closure-compiler-service - compile scripts with the Closure compiler service
+ * https://github.com/gavinhungry/closure-compiler-service
+ */
+
 (function() {
   'use strict';
 
   var ccs = {};
 
+  var fs = require('fs');
   var request = require('request');
   var querystring = require('querystring');
 
@@ -14,15 +20,25 @@
     output_info: ['compiled_code', 'errors']
   });
 
+  var die = function(msg) {
+    console.error(msg);
+    process.exit(1);
+  };
+
   /**
    * Compile a string of JavaScript with the Closure compiler service
    *
    * @param {String} js_code - JavaScript code to compile
-   * @param {Object} [opts] - API options
-   * @param {Function} callback(errors, code)
+   * @param {Object} [options] - API options
+   * @param {Function} callback(errs, code)
    */
-  ccs.compile = function(js_code, opts, callback) {
-    opts = opts || {};
+  ccs.compile = function(js_code, options, callback) {
+    var opts = options || {};
+    if (typeof options === 'function') {
+      opts = {};
+      callback = options;
+    }
+
     opts.compilation_level = opts.compilation_level || 'SIMPLE_OPTIMIZATIONS';
     opts.output_format = opts.output_format || 'json';
 
@@ -37,11 +53,25 @@
         ex = ex;
       }
 
-      var errors = (result && result.errors) ? result.errors : ex;
+      var errs = (result && result.errors) ? result.errors : ex;
       var code = (result && result.compiledCode) ? result.compiledCode : '';
-      callback(errors, code);
+      callback(errs, code);
     }).form(opts);
   };
+
+  // write output to console if called from command line
+  if (process.argv.length === 3) {
+    var filename = process.argv[2];
+
+    var js_code = fs.readFile(filename, function(err, buf) {
+      if (err) { die(err); }
+
+      ccs.compile(buf.toString(), function(errs, code) {
+        if (errs) { die(errs); }
+        console.log(code);
+      });
+    });
+  }
 
   module.exports = ccs;
 })();
